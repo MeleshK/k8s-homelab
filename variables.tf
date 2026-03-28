c755291119baca1642fb9493d928c8f4e06bc463
MeleshK <YOUR_GITHUB_NOREPLY_EMAIL>
2026-03-28 08:49:57 +0000

    update variables

--- variables.tf
+++ variables.tf
@@ -1,18 +1,42 @@
-variable "proxmox_api_url"    { type = string }
-variable "proxmox_user"       { type = string }
-variable "proxmox_password"   { type = string; sensitive = true }
-variable "proxmox_node"       { type = string; default = "pve" }
-variable "template_id"        { type = number; default = 9000 }
-variable "ssh_public_key"     { type = string }  # contents of ~/.ssh/id_ed25519.pub
-variable "worker_count"       { type = number; default = 2 }
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_user" {
+  type = string
+}
+
+variable "proxmox_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "proxmox_node" {
+  type    = string
+  default = "pve"
+}
+
+variable "template_id" {
+  type    = number
+  default = 9000
+}
+
+variable "ssh_public_key" {
+  type = string
+}
+
+variable "worker_count" {
+  type    = number
+  default = 2
+}
 
 variable "control_plane" {
   type = object({
     cores  = number
     memory = number
     disk   = string
-    ip     = string  # static: "10.0.0.10/24"
-    gw     = string  # "10.0.0.1"
+    ip     = string
+    gw     = string
   })
   default = {
     cores  = 2
@@ -24,6 +48,14 @@ variable "control_plane" {
 }
 
 variable "worker" {
-  type = object({ cores = number; memory = number; disk = string })
-  default = { cores = 2; memory = 4096; disk = "30G" }
+  type = object({
+    cores  = number
+    memory = number
+    disk   = string
+  })
+  default = {
+    cores  = 2
+    memory = 4096
+    disk   = "30G"
+  }
 }
