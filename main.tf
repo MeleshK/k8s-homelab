50b4e5710c786b81430441296588f5b8b18c2fdc
MeleshK <YOUR_GITHUB_NOREPLY_EMAIL>
2026-03-28 11:13:18 +0000

    add ssh support

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "proxmox_virtual_environment_file" "control_plane_cloud_init" {
       pod_cidr         = var.pod_cidr
       control_plane_ip = split("/", var.control_plane.ip)[0]
       vm_password      = var.vm_password
+      ssh_public_key   = var.ssh_public_key
     })
     file_name = "k8s-control-plane-init.yaml"
   }
@@ -27,8 +28,9 @@ resource "proxmox_virtual_environment_file" "worker_cloud_init" {
 
   source_raw {
     data = templatefile("${path.module}/${local.worker_script}", {
-      k8s_version = var.k8s_version
-      vm_password = var.vm_password
+      k8s_version    = var.k8s_version
+      vm_password    = var.vm_password
+      ssh_public_key = var.ssh_public_key
     })
     file_name = "k8s-worker-init.yaml"
   }
